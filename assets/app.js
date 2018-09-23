e6a373537bc69579ca26e0a6b34b21028279c15c
mjbenefiel <michaeljbenefiel@gmail.com>
2018-09-23 16:48:49 -0500

    09232018 commits-3

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,33 @@ console.log("ready")
 var currentTime = moment();
 console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
+var submitButton = $('#submit-button');
+var table = $('#train-schedule-table')
+var currentDate = new Date();
+console.log(currentDate);
+submitButton.on('click', function (event) {
+    console.log('clicked');
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var firstTrainTime = $('#firstTrainTime').val().trim();
+    var frequency = $('#frequency').val().trim();
+    var newRow = `
+<tr>
+    <td>${trainName}</td>
+    <td>${destination}</td>
+    <td>${frequency}</td>  
+    <td>${firstTrainTime}</td>
+      
+  
+</tr>
+`
+    table.append(newRow);
+});
+
+
+
+
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB1_cx3O08SnICMLnnQ-VLcDbTZHFUXWyg",
